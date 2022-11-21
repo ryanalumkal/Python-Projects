98e7d937c8f0b5d2a8bbeb05aec0839d4c7f44a6
5late <christianrfernandes5@gmail.com>
2022-11-20 22:31:05 -0500

    added year to date

--- stockmarketanalysis/main.py
+++ stockmarketanalysis/main.py
@@ -11,16 +11,23 @@ URL = [
 "https://www.google.com/finance/quote/GOOGL:NASDAQ?hl=en"
 ]
 
-def main():
+def main(url):
     soup = BeautifulSoup(page.text, "html.parser")
     company = soup.find('div',{'class': 'zzDege'}).text
     price = soup.find('div', {'class': 'YMlKec fxKbKc'} ).text
     after_hours_down = soup.find('span', {'class': 'JwB6zf'}).text # after hours percentage, change
+
+    ytd_soup = BeautifulSoup(requests.get(url + '&window=YTD').text, "html.parser")
+    # Prices
+    YTD = ytd_soup.find('div', {'class': 'JwB6zf'}).text
+
     print(f"\nCompany: {company}")
     print(f"Price: {price}")
     print(f"After Hours:{after_hours_down}")
+    print(f"Year To Date: {YTD}")
+
 
 if __name__ == '__main__':
     for i in range(len(URL)):
         page = requests.get(URL[i])
-        main()
+        main(URL[i])
