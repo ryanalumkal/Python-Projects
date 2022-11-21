892bcebf6b44ff8b2b093cce504f8e89726ead07
Ryan A <alumkalryan@gmail.com>
2022-11-20 22:27:05 -0500

    fixed issues and added more companies

--- stockmarketanalysis/main.py
+++ stockmarketanalysis/main.py
@@ -3,22 +3,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-#URL = "https://www.google.com/finance/quote/AAPL:NASDAQ?hl=en"
 URL = [
 "https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en", 
 "https://www.google.com/finance/quote/AAPL:NASDAQ?hl=en",
 "https://www.google.com/finance/quote/NFLX:NASDAQ?hl=en",
-"https://www.google.com/finance/quote/GOOGL:NASDAQ?hl=en"
+"https://www.google.com/finance/quote/GOOGL:NASDAQ?hl=en",
+"https://www.google.com/finance/quote/SHOP:NYSE?hl=en",
+"https://www.google.com/finance/quote/FAZE:NASDAQ?hl=en",
+"https://www.google.com/finance/quote/BTC-USD?hl=en"
 ]
 
 def main():
-    soup = BeautifulSoup(page.text, "html.parser")
-    company = soup.find('div',{'class': 'zzDege'}).text
-    price = soup.find('div', {'class': 'YMlKec fxKbKc'} ).text
-    after_hours_down = soup.find('span', {'class': 'JwB6zf'}).text # after hours percentage, change
-    print(f"\nCompany: {company}")
-    print(f"Price: {price}")
-    print(f"After Hours:{after_hours_down}")
+    try:
+        soup = BeautifulSoup(page.text, "html.parser")
+        company = soup.find('div',{'class': 'zzDege'}).text
+        price = soup.find('div', {'class': 'YMlKec fxKbKc'} ).text
+        after_hours_down = soup.find('span', {'class': 'JwB6zf'}).text # after hours percentage, change
+        print(f"\nCompany: {company}")
+        print(f"Price: {price}")
+        print(f"After Hours:{after_hours_down}")
+    except:
+        print(f"\nError with {URL}")
 
 if __name__ == '__main__':
     for i in range(len(URL)):
