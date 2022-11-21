3081964ca71930b53ee74ff9de51d9184d5c77f8
5late <christianrfernandes5@gmail.com>
2022-11-20 22:35:43 -0500

    fixed some stuff up

--- stockmarketanalysis/main.py
+++ stockmarketanalysis/main.py
@@ -3,7 +3,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-#URL = "https://www.google.com/finance/quote/AAPL:NASDAQ?hl=en"
 URL = [
 "https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en", 
 "https://www.google.com/finance/quote/AAPL:NASDAQ?hl=en",
@@ -11,23 +10,27 @@ URL = [
 "https://www.google.com/finance/quote/GOOGL:NASDAQ?hl=en"
 ]
 
-def main(url):
-    soup = BeautifulSoup(page.text, "html.parser")
-    company = soup.find('div',{'class': 'zzDege'}).text
-    price = soup.find('div', {'class': 'YMlKec fxKbKc'} ).text
-    after_hours_down = soup.find('span', {'class': 'JwB6zf'}).text # after hours percentage, change
+def main():
+    try:
+        soup = BeautifulSoup(page.text, "html.parser")
+        company = soup.find('div',{'class': 'zzDege'}).text
+        price = soup.find('div', {'class': 'YMlKec fxKbKc'} ).text
+        after_hours_down = soup.find('span', {'class': 'JwB6zf'}).text # after hours percentage, change
 
-    ytd_soup = BeautifulSoup(requests.get(url + '&window=YTD').text, "html.parser")
+        ytd_soup = BeautifulSoup(requests.get(URL[i] + '&window=YTD').text, "html.parser")
     # Prices
-    YTD = ytd_soup.find('div', {'class': 'JwB6zf'}).text
+        YTD = ytd_soup.find('div', {'class': 'JwB6zf'}).text
 
-    print(f"\nCompany: {company}")
-    print(f"Price: {price}")
-    print(f"After Hours:{after_hours_down}")
-    print(f"Year To Date: {YTD}")
+        print(f"\nCompany: {company}")
+        print(f"Price: {price}")
+        print(f"After Hours:{after_hours_down}")
+        print(f"Year To Date: {YTD}")
+
+    except:
+        print(f"Error with {URL[i]}")
 
 
 if __name__ == '__main__':
     for i in range(len(URL)):
         page = requests.get(URL[i])
-        main(URL[i])
+        main()
